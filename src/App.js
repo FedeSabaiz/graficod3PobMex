d1d58d93c4f84d896cfd4c52d1b39952462fb408
Federico <federico.gomez.sa@gmail.com>
2019-09-11 18:57:41 -0500

    Actualizando app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/custom.scss';
+import GraficoInai from './components/GraficoInai';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			resultado: []
+		};
+		
+				
+	}
+	
+	async informacion() {
+		let data = await fetch('http://api.worldbank.org/v2/country/mx/indicator/SP.POP.TOTL?format=json').then(res => res.json());
+		// console.log(data[1]);
+		let resultado = data[1].map(x => {
+			return {year:x.date, people:x.value}
+		})
+		this.setState({
+			resultado
+		})
+	}
+
+	componentDidMount() {
+		this.informacion();
+	}
+	
+	
+	render() { 
+		
+		
+		return ( 
+				<div>
+					<h1>Panel de control</h1>
+				
+					<GraficoInai data={this.state.resultado}/>
+				</div>
+		);
+	}
 }
-
+ 
 export default App;
